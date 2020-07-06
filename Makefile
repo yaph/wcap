2f96e434b59bf5fb9099135b99d979bd5b8f8c89
Ramiro Gómez <code@ramiro.org>
2020-07-07 00:14:29 +0200

    Add and use get_browser function

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 .PHONY: clean clean-test clean-pyc clean-build docs help
 
+
 clean: clean-build clean-pyc clean-test ## remove all build, test, coverage and Python artifacts
+
 
 clean-build: ## remove build artifacts
 	rm -fr build/
@@ -9,11 +11,13 @@ clean-build: ## remove build artifacts
 	find . -name '*.egg-info' -exec rm -fr {} +
 	find . -name '*.egg' -exec rm -f {} +
 
+
 clean-pyc: ## remove Python file artifacts
 	find . -name '*.pyc' -exec rm -f {} +
 	find . -name '*.pyo' -exec rm -f {} +
 	find . -name '*~' -exec rm -f {} +
 	find . -name '__pycache__' -exec rm -fr {} +
+
 
 clean-test: ## remove test and coverage artifacts
 	rm -fr .tox/
@@ -21,13 +25,22 @@ clean-test: ## remove test and coverage artifacts
 	rm -fr htmlcov/
 	rm -fr .pytest_cache
 
-release: dist ## package and upload a release
-	twine upload dist/*
 
 dist: clean ## builds source and wheel package
 	python setup.py sdist
 	python setup.py bdist_wheel
 	ls -l dist
 
+
 install: clean ## install the package to the active Python's site-packages
 	python setup.py install
+
+
+release: dist ## package and upload a release
+	twine upload dist/*
+
+
+test:
+	wcap --dimensions 1500x1100 --wait 5 https://ukealong.com/key/c/ img/c.png
+	wcap --dimensions 1500x1100 https://ukealong.com/key/d/ img/d.png
+	wcap https://ukealong.com/key/e/ img/e.png
--- wcap/wcap.py
+++ wcap/wcap.py
@@ -10,20 +10,19 @@ DIMENSIONS = '1280x800'  # Default image dimensions width x height
 WAIT = 2  # Default wait time to render web page
 
 
-def capture(url: str, image: str, dimensions: str = DIMENSIONS, wait: int = WAIT) -> None:
-    width, height = [int(dim) for dim in dimensions.split('x')]
-
+def get_browser() -> webdriver:
     options = webdriver.FirefoxOptions()
     options.add_argument('-headless')
+    return webdriver.Firefox(options=options)
 
-    browser = webdriver.Firefox(options=options)
+
+def capture(browser: webdriver, url: str, image: str, dimensions: str = DIMENSIONS, wait: int = WAIT) -> None:
+    width, height = [int(dim) for dim in dimensions.split('x')]
     browser.set_window_size(width, height)
     browser.get(url)
     browser.set_window_rect(x=0, y=0, width=width, height=height)
-
     time.sleep(wait)
     browser.save_screenshot(image)
-    browser.close()
 
 
 def main():
@@ -32,8 +31,11 @@ def main():
     parser.add_argument('image', metavar='IMAGE', type=str, help='Image file name')
     parser.add_argument('--dimensions', '-d', type=str, default=DIMENSIONS)
     parser.add_argument('--wait', '-w', type=int, default=WAIT)
+
     args = parser.parse_args()
-    capture(args.url, args.image, args.dimensions, args.wait)
+    browser = get_browser()
+    capture(browser, args.url, args.image, args.dimensions, args.wait)
+    browser.close()
 
 
 if __name__ == '__main__':
