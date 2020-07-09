25d0a5497b3f24d632efcef431536f12b1fa08d7
Ramiro Gómez <code@ramiro.org>
2020-07-09 21:19:27 +0200

    Expose get_browser and capture in __init__.py

--- wcap/__init__.py
+++ wcap/__init__.py
@@ -3,6 +3,7 @@ __version__ = '0.2.0'
 __author__ = 'Ramiro Gómez <code@ramiro.org>'
 __name__ = 'wcap'
 __description__ = 'Capture web pages as images using Selenium and the FireFox webdriver.'
+__all__ = ['get_browser', 'capture']
 
 
 from .wcap import get_browser, capture
